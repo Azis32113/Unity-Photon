e56332551b053e32bb7cca807a0cde4c9a05b555
Azis <amirulloh.azis@gmail.com>
2024-06-03 17:08:59 +0700

    Set Name using RPC

--- Assets/Scripts/Runtime/App.cs
+++ Assets/Scripts/Runtime/App.cs
@@ -61,6 +61,7 @@ public class App : MonoBehaviour
         if (GateUI(uiRoom)) {
             PlayerPrefs.SetString(Constants.ServerData.GAME_MODE, gameMode.ToString());
             PlayerPrefs.SetString(Constants.ServerData.SESSION_NAME, serverInputField.text);
+            PlayerPrefs.SetString(Constants.LocalData.PLAYER_NICK_NAME, nameInputField.text);
             PlayerPrefs.Save();
             SceneManager.LoadSceneAsync(Constants.Scene.GAME);
         }
